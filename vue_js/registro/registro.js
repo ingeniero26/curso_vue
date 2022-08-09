17e2d91e3434609e142bb797396ca27c95476c2f
ingeniero26 <ingjerson2014@gmail.com>
2022-08-09 10:21:35 -0500

    creamos un crud basico de clientes

---
+++ vue_js/registro/registro.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        titulo:'REGISTRO DE CLIENTES',
+        clientes:[],
+        nombre:'',
+        apellidos:'',
+        documento:'',
+        direccion:'',
+        nuevoCliente:'',
+        nuevoApellidos:'',
+        nuevoDoc:'',
+        nuevaDir:''
+        
+
+    },
+    methods: {
+        AgregarCliente() {
+            this.clientes.push({
+                nombre:this.nuevoCliente,
+                 apellidos:this.nuevoApellidos,
+                 documento:this.nuevoDoc,
+                 direccion:this.nuevaDir,
+                 estado:false
+
+            }),
+            this.nuevoCliente ='';
+            this.nuevoApellidos='';
+            this.nuevoDoc='';
+            this.nuevaDir='';
+            localStorage.setItem('cliente-local',JSON.stringify(this.clientes));
+        },
+        editEstado: function(index) {
+            this.clientes[index].estado = true;
+            localStorage.setItem('cliente-local',JSON.stringify(this.clientes));
+        },
+        deleteCliente:function(index) {
+            this.clientes.splice(index,1);
+            localStorage.setItem('cliente-local',JSON.stringify(this.clientes));
+        }
+    },
+    created: function() {
+        let datosDB = JSON.parse(localStorage.getItem('cliente-local'));
+      //  console.log(datosDB);
+      if(datosDB === null) {
+        this.clientes =[];
+      } else {
+        this.clientes = datosDB;
+      }
+    },
+});
